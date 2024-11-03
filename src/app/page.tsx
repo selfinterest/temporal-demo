da2734862dcc2c1a15129a772f1c7d93c8370e37
Kate Watson <kate@abridge.com>
2024-11-03 16:43:39 -0500

    with api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,70 @@
 "use client";
-import { QueryClient, QueryClientProvider } from "react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useMutation,
+} from "@tanstack/react-query";
 import Image from "next/image";
 import { useState } from "react";
 
-const queryClient = new QueryClient();
-
 export default function Home() {
-  const verifyGameAndRedirect = () => {};
+  async function fetchGame() {
+    const response = await fetch(`/api/game/verify/${gameId}`);
+    const data = await response.json();
+    return data;
+  }
+
+  const mutation = useMutation({
+    mutationFn: fetchGame,
+    onSuccess: (data) => {
+      // Handle the data on success
+      console.log("API Response:", data);
+    },
+    onError: (error) => {
+      // Handle any errors
+      console.error("Error fetching data:", error);
+    },
+  });
+
+  const verifyGameAndRedirect = () => {
+    mutation.mutate();
+  };
+
   const [gameId, setGameId] = useState("");
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <Image
-            className="dark:invert"
-            src="/dollar.jpg"
-            alt="Money"
-            width={800}
-            height={38}
-            priority
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/dollar.jpg"
+          alt="Money"
+          width={800}
+          height={38}
+          priority
+        />
+        <h1 className="hb-4 text-4xl font-extrabold leading-none tracking-tight text-center">
+          Katey&apos;s Divide-the-Money Game
+        </h1>
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <input
+            type="text"
+            className="border border-gray-300 p-2 rounded-md flex-1"
+            placeholder="Game ID"
+            onChange={(e) => setGameId(e.target.value?.toUpperCase())}
+            value={gameId}
           />
-          <h1 className="hb-4 text-4xl font-extrabold leading-none tracking-tight text-center">
-            Katey&apos;s Divide-the-Money Game
-          </h1>
-          <div className="flex gap-4 items-center flex-col sm:flex-row">
-            <input
-              type="text"
-              className="border border-gray-300 p-2 rounded-md flex-1"
-              placeholder="Game ID"
-              onChange={(e) => setGameId(e.target.value?.toUpperCase())}
-              value={gameId}
-            />
-            <button
-              className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
-              onClick={verifyGameAndRedirect}
-            >
-              Submit
-            </button>
-          </div>
-        </main>
-        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-          <div>&copy; Katey Watson 2024</div>
-        </footer>
-      </div>
-    </QueryClientProvider>
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+            onClick={verifyGameAndRedirect}
+          >
+            Submit
+          </button>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <div>&copy; Katey Watson 2024</div>
+      </footer>
+    </div>
   );
 }
---
+++ src/pages/api/game/verify/[id].ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import * as DynamoDB from "@aws-sdk/client-dynamodb";
+import { awsCredentialsProvider } from "@vercel/functions/oidc";
+
+const AWS_REGION = process.env.AWS_REGION || "us-east-1";
+const AWS_ROLE_ARN = process.env.AWS_ROLE_ARN!;
+
+const dynamoClient = new DynamoDB.DynamoDBClient({
+  region: AWS_REGION,
+  // Use the Vercel AWS SDK credentials provider
+  credentials: awsCredentialsProvider({
+    roleArn: AWS_ROLE_ARN,
+  }),
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    const gameId = req.query.id;
+    if (!gameId || Array.isArray(gameId)) {
+      res.status(400).end(`Invalid request`);
+    } else {
+      const command = new DynamoDB.GetItemCommand({
+        TableName: "MoneyGames",
+        Key: { id: { S: gameId } },
+      });
+      const data = await dynamoClient.send(command);
+      return data.Item;
+    }
+    // Handle GET request
+    //res.status(200).json({ message: "This is a GET request" });
+  } else {
+    // Handle other methods or throw an error
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
