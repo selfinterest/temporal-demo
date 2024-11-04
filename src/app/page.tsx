4274d994227bb37e167eb9252c3e4882dc6c71a0
Kate Watson <kate@abridge.com>
2024-11-03 21:49:23 -0500

    try again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,14 +31,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/dollar.jpg"
-          alt="Money"
-          width={800}
-          height={38}
-          priority
-        />
+        <Image src="/dollar.jpg" alt="Money" width={800} height={38} priority />
         <h1 className="hb-4 text-4xl font-extrabold leading-none tracking-tight text-center">
           Katey&apos;s Divide-the-Money Game
         </h1>
